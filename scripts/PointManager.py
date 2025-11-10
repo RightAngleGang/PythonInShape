f92463912a27c83fbadf7836c1590d9c0e5f56e3
ColinLeDev <github@col1n.fr>
2025-11-10 11:04:26 +0100

    Add struct for string casting add number_of_points, remove_point, find_point_by_name

--- scripts/PointManager.py
+++ scripts/PointManager.py
@@ -7,6 +7,9 @@ class PointManager:
 
     def __init__(self):
         self.points = []
+    
+    def __str__(self):
+        pass
 
     def add_point(self, point: Point):
         self.points.append(point)
@@ -14,5 +17,20 @@ class PointManager:
     def get_points(self) -> list[Point]:
         """Retourne la liste des points"""
         return self.points
+    
+    def number_of_points(self) -> int:
+        """Retourne le nombre de points"""
+        return len(self.points)
+        
+    def remove_point(self, point: Point):
+        """Supprime un point"""
+        self.points.remove(point)
+        
+    def find_point_by_name(self, name: str) -> Point | None:
+        """Recherche un point par son nom"""
+        for point in self.points:
+            if point.nom == name:
+                return point
+        return None
     
     
