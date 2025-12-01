19c14fa37bfb1a30619369b6523bf0a8d1a59578
ColinLeDev <github@col1n.fr>
2025-12-01 15:51:38 +0100

    Add first version of eq for a polygon

--- scripts/Polygon.py
+++ scripts/Polygon.py
@@ -12,7 +12,18 @@ class Polygon(Shape):
 
     def __str__(self):
         return f"{self.nom} (polygone): [{'; '.join(str(p) for p in self.points)}]"
-
+    
+    def __eq__(self, value: "Polygon") -> bool:
+        if not isinstance(value, Polygon):
+            return False
+        
+        for point in self.points:
+            if point not in value.points:
+                return False
+        
+        return len(self.points) != len(value.points)
+    
+    
     def add_point(self, point: Point):
         self.points.append(point)
         
